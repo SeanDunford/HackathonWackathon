51d8e8d7282f4994daf762e37e9640b366ea31b8
Sean Dunford <sdunford@mail.usf.edu>
2014-11-05 19:50:16 -0800

    Just keep hacking and wacking and smacking

---
+++ Wackathon/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Wackathon
+//
+//  Created by Sean Dunford on 11/5/14.
+//  Copyright (c) 2014 LuqSean. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
