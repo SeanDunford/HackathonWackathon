929eec802f129f15e6d2ce3a2a20714a72043d93
Sean Dunford <sdunford@mail.usf.edu>
2014-11-10 22:03:27 -0800

    Basic pedometer and stair tracking

--- Wackathon/AppPages/M7ViewController.swift
+++ Wackathon/AppPages/M7ViewController.swift
@@ -10,12 +10,13 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class M7ViewController: UIViewController {
 
 
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.redColor(); 
     }
 
 
